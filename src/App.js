f0975af4c6b2fb7c69f0c2504be9932101f0c674
Grazvis <grazvydas.untulis@gmail.com>
2022-05-09 18:45:54 +0300

    Routing implemented(react-router-dom)

--- src/App.js
+++ src/App.js
@@ -1,8 +1,23 @@
-import logo from './logo.svg';
-import './App.css';
+import Header from './components/Header'
+import Footer from './components/Footer'
+import List from './components/List'
+import ListItem from './components/ListItem'
+import { Route, Switch } from 'react-router-dom'
 
 function App() {
   return (
+    <>
+      <Header />
+        <Switch>
+          <Route path="/">
+            <List />
+          </Route>
+          <Route path="/:id">
+            <ListItem />
+          </Route>
+        </Switch>
+      <Footer />
+    </>
   );
 }
 
