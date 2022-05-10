4bddb41565193fd952ebefa3c736827ca8b12cbc
Grazvis <grazvydas.untulis@gmail.com>
2022-05-10 21:49:40 +0300

    Table component added

--- src/components/ListTabel.jsx
+++ src/components/ListTabel.jsx
@@ -1,9 +1,235 @@
-const ListTabel = () => {
-  return (
-    <>
-    </>
-  )
+import * as React from 'react';
+import Box from '@mui/material/Box';
+import Table from '@mui/material/Table';
+import TableBody from '@mui/material/TableBody';
+import TableCell from '@mui/material/TableCell';
+import TableContainer from '@mui/material/TableContainer';
+import TableHead from '@mui/material/TableHead';
+import TablePagination from '@mui/material/TablePagination';
+import TableRow from '@mui/material/TableRow';
+import TableSortLabel from '@mui/material/TableSortLabel';
+import Typography from '@mui/material/Typography';
+import Paper from '@mui/material/Paper';
+import FormControlLabel from '@mui/material/FormControlLabel';
+import Switch from '@mui/material/Switch';
+import { visuallyHidden } from '@mui/utils';
+import { useNavigate } from "react-router-dom";
+import Grid from '@mui/material/Grid';
+
+function createData(
+  userId,
+  title,
+  body,
+  id
+){
+  return {
+    userId,
+    title,
+    body,
+    id
+  };
 }
 
-export default ListTabel
+const rows = [
+  createData(1, 'qhadsf', 'Some kind of giberish', 1),
+  createData(2, 'bomb', 'pukšt', 2),
+  createData(1, 'plane', 'Flying high', 3),
+  createData(3, 'dangus', 'creator', 4),
+  createData(4, 'war', 'shit', 5),
+  createData(5, 'List', "list of arrays is a 2D array", 5),
+  createData(1, 'demilitarizacija', 'mhmm', 6)
+];
 
+function descendingComparator(a, b, orderBy) {
+    if (b[orderBy] < a[orderBy]) {
+        return -1;
+    }
+    if (b[orderBy] > a[orderBy]) {
+        return 1;
+    }
+    return 0;
+}
+
+function getComparator(order, orderBy) {
+    return order === 'desc'
+        ? function (a, b) { return descendingComparator(a, b, orderBy); }
+        : function (a, b) { return -descendingComparator(a, b, orderBy); };
+}
+
+// This method is created for cross-browser compatibility, if you don't
+// need to support IE11, you can use Array.prototype.sort() directly
+function stableSort(array, comparator) {
+    var stabilizedThis = array.map(function (el, index) { return [el, index]; });
+    stabilizedThis.sort(function (a, b) {
+        var order = comparator(a[0], b[0]);
+        if (order !== 0) {
+            return order;
+        }
+        return a[1] - b[1];
+    });
+    return stabilizedThis.map(function (el) { return el[0]; });
+}
+
+
+var headCells = [
+    {
+        id: 'userId',
+        numeric: true,
+        disablePadding: true,
+        label: 'User ID'
+    },
+    {
+        id: 'title',
+        numeric: false,
+        disablePadding: false,
+        label: 'Title'
+    },
+    {
+        id: 'body',
+        numeric: true,
+        disablePadding: false,
+        label: 'Body'
+    }
+];
+
+
+function EnhancedTableHead(props) {
+  const { order, orderBy, onRequestSort } = props;
+  const createSortHandler = (property) => (event) => {
+      onRequestSort(event, property);
+    };
+
+  return (
+    <TableHead>
+      <TableRow>
+        {headCells.map((headCell) => (
+          <TableCell
+            key={headCell.id}
+            align={headCell.numeric ? 'right' : 'left'}
+            padding={headCell.disablePadding ? 'none' : 'normal'}
+            sortDirection={orderBy === headCell.id ? order : false}
+          >
+            <TableSortLabel
+              active={orderBy === headCell.id}
+              direction={orderBy === headCell.id ? order : 'asc'}
+              onClick={createSortHandler(headCell.id)}
+            >
+              {headCell.label}
+              {orderBy === headCell.id ? (
+                <Box component="span" sx={visuallyHidden}>
+                  {order === 'desc' ? 'sorted descending' : 'sorted ascending'}
+                </Box>
+              ) : null}
+            </TableSortLabel>
+          </TableCell>
+        ))}
+      </TableRow>
+    </TableHead>
+  );
+}
+
+const EnhancedTable = () => {
+  const [order, setOrder] = React.useState('asc');
+  const [orderBy, setOrderBy] = React.useState('title');
+  const [page, setPage] = React.useState(0);
+  const [dense, setDense] = React.useState(false);
+  const [rowsPerPage, setRowsPerPage] = React.useState(5);
+  const navigate = useNavigate();
+
+  const handleRequestSort = (
+    event,
+    property,
+  ) => {
+    const isAsc = orderBy === property && order === 'asc';
+    setOrder(isAsc ? 'desc' : 'asc');
+    setOrderBy(property);
+  };
+
+  const handleClick = (event, id) => {
+    navigate(`/item/${id}`)
+  };
+
+  const handleChangePage = (event, newPage) => {
+    setPage(newPage);
+  };
+
+  const handleChangeRowsPerPage = (event) => {
+    setRowsPerPage(parseInt(event.target.value, 10));
+    setPage(0);
+  };
+
+  const handleChangeDense = (event) => {
+    setDense(event.target.checked);
+  };
+
+  // Avoid a layout jump when reaching the last page with empty rows.
+  const emptyRows =
+    page > 0 ? Math.max(0, (1 + page) * rowsPerPage - rows.length) : 0;
+
+  return (
+    <Grid container justifyContent="center">
+    <Box sx={{ width: '80%'}}>
+      <Paper sx={{ width: '100%', my: 2 }}>
+        <TableContainer>
+          <Table
+            sx={{ minWidth: 750 }}
+            aria-labelledby="tableTitle"
+            size={dense ? 'small' : 'medium'}
+          >
+            <EnhancedTableHead
+              order={order}
+              orderBy={orderBy}
+              onRequestSort={handleRequestSort}
+            />
+            <TableBody>
+              {/* if you don't need to support IE11, you can replace the `stableSort` call with:
+              rows.slice().sort(getComparator(order, orderBy)) */}
+              {stableSort(rows, getComparator(order, orderBy))
+                .slice(page * rowsPerPage, page * rowsPerPage + rowsPerPage)
+                .map((row, index) => {
+                  const labelId = `enhanced-table-${index}`;
+
+                  return (
+                    <TableRow
+                      hover
+                      onClick={(event) => handleClick(event, row.id)}
+                      tabIndex={-1}
+                      key={row.name}
+                    >
+                      <TableCell align="right" sx={{ width: '10%' }}>{row.userId}</TableCell>
+                      <TableCell align="left">{row.title}</TableCell>
+                      <TableCell align="right">{row.body}</TableCell>
+                    </TableRow>
+                  );
+                })}
+              {emptyRows > 0 && (
+                <TableRow
+                  style={{
+                    height: (dense ? 33 : 53) * emptyRows,
+                  }}
+                >
+                </TableRow>
+              )}
+            </TableBody>
+          </Table>
+        </TableContainer>
+        <TablePagination
+          rowsPerPageOptions={[5, 10, 25]}
+          component="div"
+          count={rows.length}
+          rowsPerPage={rowsPerPage}
+          page={page}
+          onPageChange={handleChangePage}
+          onRowsPerPageChange={handleChangeRowsPerPage}
+        />
+      </Paper>
+      <FormControlLabel
+        control={<Switch checked={dense} onChange={handleChangeDense} />}
+        label="Dense padding"
+      />
+    </Box>
+  </Grid>
+  );
+}
+
+export default EnhancedTable 
