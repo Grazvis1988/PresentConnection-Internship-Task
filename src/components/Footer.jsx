f2953c712f3b3045b077b819548ac36ecac85922
Grazvis <grazvydas.untulis@gmail.com>
2022-05-11 17:28:16 +0300

    Footer added

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,7 +1,13 @@
+import { 
+  Box
+} from '@mui/material'
 const Footer = () => {
   return (
-    <>
-    </>
+    <footer style={{ color: 'rgba(223, 209, 209, 1)', position: 'fixed', bottom: 0, width: '100%' }}>
+      <Box textAlign='center' >
+        Copyright &reg; {new Date().getFullYear()}
+      </Box>
+    </footer>
   )
 }
 
